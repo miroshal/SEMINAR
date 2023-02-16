c51a90bbf38dc330c3d44a26478ce03ad9d43b8b
AndreyMir <miroshal@mail.ru>
2023-02-16 04:48:14 +0300

    Дз4

---
+++ C#/sem1/dz4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.WriteLine("Введите три числа: ");
+Console.Write ("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
