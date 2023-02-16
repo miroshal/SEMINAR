110fb2f2880ad63bf65e6f8e9fa1d7c5779771c2
AndreyMir <miroshal@mail.ru>
2023-02-16 05:45:47 +0300

    Дз8

---
+++ C#/sem1/dz8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write ("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 2;
+
+if (N % 2 == 0)
+{
+    while (a < N + 1)
+  {
+    Console.Write ($"{a} ");
+    a = a + 2;
+  }
+}
+else
+{
+    while (a < N)
+  {
+    Console.Write ($"{a} ");
+    a = a + 2;
+  }
+}
